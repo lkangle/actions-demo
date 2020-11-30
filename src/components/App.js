9c1aa40543bac776da5eacf071616ae4889fbaa7
likangle <likangle@growingio.com>
2020-11-30 22:24:34 +0800

    fix: 添加自动部署到ssh测试

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='app'>
+      <h2>
+        React Github Actions Test
+      </h2>
+    </div>
+  );
+}
+
+export default App;
